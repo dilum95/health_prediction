72f191c1dd85bd651a98a53ff1ce7c45b1f029c6
Dilum Darshana <dilum@localhost.localdomain>
2022-10-10 12:05:43 -0400

    final update

--- Code/Server/lib/db.config.js
+++ Code/Server/lib/db.config.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb'
 
-const uri = "mongodb+srv://dilum95:19524061@cluster0.rqcuqyb.mongodb.net/?retryWrites=true&w=majority"
+const uri = ""
 
 
 const options = {
